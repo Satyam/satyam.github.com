fa9c50eede1d9c7571582ccbe3596099253f8802
Daniel Barreiro <satyam@satyam.com.ar>
2011-08-23 15:35:46 +0200

    Fixed the name of the button from button to gallery-md-button

--- apiDocsGallery/classmap.js
+++ apiDocsGallery/classmap.js
@@ -1,4 +1,4 @@
-YAHOO.env.classMap = {"ButtonGroup": "gallery-md-button-group", "AccordionPanel": "gallery-md-accordion", "Accordion": "gallery-md-accordion", "Button": "button", "ButtonToggle": "button", "Spinner": "gallery-md-spinner", "ButtonSeparator": "gallery-md-button-group", "MakeNode": "gallery-md-makenode", "TimeSpinner": "gallery-md-timespinner"};
+YAHOO.env.classMap = {"ButtonGroup": "gallery-md-button-group", "AccordionPanel": "gallery-md-accordion", "Accordion": "gallery-md-accordion", "Button": "gallery-md-button", "ButtonToggle": "gallery-md-button", "Spinner": "gallery-md-spinner", "ButtonSeparator": "gallery-md-button-group", "MakeNode": "gallery-md-makenode", "TimeSpinner": "gallery-md-timespinner"};
 
 YAHOO.env.resolveClass = function(className) {
     var a=className.split('.'), ns=YAHOO.env.classMap;
