c8e78a4835613f497a3d74aa1dddbd0523f215dd
Daniel Barreiro <satyam@satyam.com.ar>
2011-08-23 13:56:32 +0200

    first commit

---
+++ apiDocsGallery/classmap.js
@@ -0,0 +1,15 @@
+YAHOO.env.classMap = {"ButtonGroup": "gallery-md-button-group", "AccordionPanel": "gallery-md-accordion", "Accordion": "gallery-md-accordion", "Button": "button", "ButtonToggle": "button", "Spinner": "gallery-md-spinner", "ButtonSeparator": "gallery-md-button-group", "MakeNode": "gallery-md-makenode", "TimeSpinner": "gallery-md-timespinner"};
+
+YAHOO.env.resolveClass = function(className) {
+    var a=className.split('.'), ns=YAHOO.env.classMap;
+
+    for (var i=0; i<a.length; i=i+1) {
+        if (ns[a[i]]) {
+            ns = ns[a[i]];
+        } else {
+            return null;
+        }
+    }
+
+    return ns;
+};
