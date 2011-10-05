10d14a81ce99b4cb07f546856f4ed4f99f15f681
Daniel Barreiro <satyam@satyam.com.ar>
2011-10-05 10:30:45 +0200

    Added docs for Gallery-StdMod and Gallery IButton

--- apiDocsGallery/classmap.js
+++ apiDocsGallery/classmap.js
@@ -1,4 +1,4 @@
-YAHOO.env.classMap = {"ButtonGroup": "gallery-md-button-group", "AccordionPanel": "gallery-md-accordion", "Accordion": "gallery-md-accordion", "Button": "gallery-md-button", "ButtonToggle": "gallery-md-button", "Spinner": "gallery-md-spinner", "ButtonSeparator": "gallery-md-button-group", "MakeNode": "gallery-makenode", "TimeSpinner": "gallery-md-timespinner"};
+YAHOO.env.classMap = {"ButtonGroup": "gallery-md-button-group", "AccordionPanel": "gallery-md-accordion", "Accordion": "gallery-md-accordion", "Button": "gallery-md-button", "StdMod": "gallery-stdmod", "ButtonToggle": "gallery-md-button", "Spinner": "gallery-md-spinner", "ButtonSeparator": "gallery-md-button-group", "IButton": "gallery-md-ibutton", "MakeNode": "gallery-makenode", "TimeSpinner": "gallery-md-timespinner"};
 
 YAHOO.env.resolveClass = function(className) {
     var a=className.split('.'), ns=YAHOO.env.classMap;
