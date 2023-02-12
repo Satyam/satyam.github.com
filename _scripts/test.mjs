e94aa5c027887996dfe7712f53f8e7caa2880da3
Satyam <satyam@satyam.com.ar>
2023-02-12 19:20:09 +0100

    Slightly improved and commented script. Includes checks to ensure all posts are listed in at least one category.

--- _scripts/test.mjs
+++ _scripts/test.mjs
@@ -25,6 +25,8 @@ const entityDecoder = (src) =>
     // console.error('unencoded', m);
     return m;
   });
+const spacesRex = /\s+/g;
+const trimSpaces = (s) => s.trim().replaceAll(spacesRex, ' ');
 /*
 // Mitos:
 {
@@ -32,7 +34,7 @@ const entityDecoder = (src) =>
   const mitosSite = path.join(destSite, '_mitos');
   fs.ensureDir(mitosSite);
   fs.emptyDir(mitosSite);
-
+  console.log('======= mitos 35 =========')
   for (const page of pages) {
     const root = parse(entityDecoder(await fs.readFile(page, 'utf8')));
     const title =
@@ -61,6 +63,7 @@ ${body}
   fs.ensureDir(milagrosSite);
   fs.emptyDir(milagrosSite);
 
+  console.log('========= milagros 64 =========')
   for (const page of pages) {
     const root = parse(entityDecoder(await fs.readFile(page, 'utf8')));
     const title =
@@ -85,8 +88,14 @@ ${lastTd}
   }
 }
 */
+
+const blogInfoRex =
+  /blog\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<slug>[^\/]+)/;
+
+const postFilenames = {};
 // posts
 {
+  console.log('========== posts 96 =========');
   const imgFiles = [];
   const postsSite = path.join(destSite, '_posts');
   fs.ensureDir(postsSite);
@@ -94,8 +103,6 @@ ${lastTd}
   const globFilenames = await glob(
     path.join(srcSite, 'blog/20*/*/*/*/index.html')
   );
-  const blogInfoRex =
-    /blog\/(?<year>\d+)\/(?<month>\d+)\/(?<day>\d+)\/(?<slug>[^\/]+)/;
   for (const name of globFilenames) {
     // console.log(name);
     const m = blogInfoRex.exec(name);
@@ -235,16 +242,13 @@ ${lastTd}
     const $postInfo = $$postInfo[0];
     const $postTitle = $postInfo.querySelector('.post-title');
     if ($postTitle) {
-      const title = entityDecoder($postTitle.textContent)
-        .trim()
-        .replaceAll(/\s+/g, ' ');
-      const category = entityDecoder(
-        $postInfo.querySelector('a[rel~="category"]').textContent
-      )
-        .trim()
-        .replaceAll(/\s+/g, ' ');
+      const title = trimSpaces(entityDecoder($postTitle.textContent));
+      const category = trimSpaces(
+        entityDecoder($postInfo.querySelector('a[rel~="category"]').textContent)
+      );
       // console.log(title, category, slug);
 
+      postFilenames[filename] = [];
       // Finally, write it!
       await fs.writeFile(
         path.join(postsSite, filename),
@@ -262,29 +266,33 @@ ${content}
 
 // Deal with nested categories
 {
+  console.log('==== nested categories 271 ==========');
   const catsSite = path.join(destSite, '_categorias');
   fs.ensureDir(catsSite);
   fs.emptyDir(catsSite);
+
+  // all pages contain the sidebar with the categories so
+  // I might as well read it from the home page.
   const $home = parse(
     entityDecoder(
       await fs.readFile(path.join(srcSite, 'blog/index.html'), 'utf8')
     )
   );
 
-  const getCat = ($li) => {
-    const $a = $li.querySelector('a');
-    return [$a.textContent, $a.getAttribute('href')];
-  };
+  const catHash = {};
 
-  const catFlat = {};
-  for (const $item of $home
+  // read all the links in the categories, read the description and the url
+  for (const $a of $home
     .getElementById('sidebar')
-    .querySelectorAll('li.cat-item')) {
-    const [name, url] = getCat($item);
+    .querySelectorAll('li.cat-item a')) {
+    // const $a = $item.querySelector('a');
+    const name = $a.textContent;
+    const url = $a.getAttribute('href');
     if (!url.startsWith('/blog/category/')) {
       console.error('strange url', name, url);
       continue;
     }
+    // make the id for the category out of the url
     const parts = url.split('/');
     const l = parts.length;
     if (l < 5 || l > 6) {
@@ -292,8 +300,8 @@ ${content}
       continue;
     }
     const p3 = parts[3];
-    if (!catFlat[p3]) {
-      catFlat[p3] = {
+    if (!catHash[p3]) {
+      catHash[p3] = {
         id: p3,
         name,
         url,
@@ -302,17 +310,23 @@ ${content}
     if (l === 6) {
       const p4 = parts[4];
       const id = [p3, p4].join('/');
-      if (!catFlat[id]) catFlat[id] = { id, name, url, parent: p3 };
+      if (!catHash[id]) catHash[id] = { id, name, url, parent: p3 };
     }
   }
-  for (const cat of Object.values(catFlat)) {
+  // Read each of the categories pages and pick info about the posts
+  // contained in the indices
+  console.log('====== looking for posts in catHash 315 ==========');
+  for (const cat of Object.values(catHash)) {
     console.log(cat.id, cat.name);
     const posts = [];
-    const parentPosts = catFlat[cat.parent]?.posts;
+    const parentPosts = catHash[cat.parent]?.posts;
+    // Big categories take several pages of indexes:
     const pages = await glob([
       path.join(srcSite, cat.url, 'index.html'),
       path.join(srcSite, cat.url, 'page/*/index.html'),
     ]);
+    // For each category index page, read the posts it contains
+    // and get the title and url of each post.
     for (const page of pages) {
       console.log(page);
       const root = parse(entityDecoder(await fs.readFile(page, 'utf8')));
@@ -324,21 +338,40 @@ ${content}
           console.error('not just one title', $$title.length);
         const $anchor = $$title[0];
         const url = $anchor.getAttribute('href');
-        const $$date = $post.querySelectorAll('.post-date');
-        if ($$date.length !== 1)
-          console.error('not just one date', $$date.length);
+
+        // Get the date and slug from the url
+        const m = blogInfoRex.exec(url);
+        // if (m) console.log(m.groups);
+        if (!m) {
+          console.error('no match', url);
+          continue;
+        }
+        const { year, month, day, slug } = m.groups;
+
         posts.push({
-          date: $$date[0].textContent.trim(),
-          title: $anchor.textContent.trim(),
+          date: [year, month, day].join('-'),
+          title: trimSpaces($anchor.textContent),
           url,
+          year,
+          month,
+          day,
+          slug,
         });
+
+        // If this post is listed under the parent category, remove it from the parent
+        // Since parent folders come listed before children, the parent will already be
+        // populated.
         if (parentPosts) {
           const i = parentPosts.findIndex((pp) => pp.url === url);
           if (i !== -1) parentPosts.splice(i, 1);
         }
       }
     }
+
+    // save those posts in the category entry
     cat.posts = posts;
+
+    // write the new category entry (only the front matter is needed)
     const filename = path.join(catsSite, `${cat.id}.html`);
     await fs.ensureFile(filename);
     await fs.writeFile(
@@ -351,5 +384,46 @@ title:  "${cat.name}"
 `
     );
   }
-  console.log('=================', JSON.stringify(catFlat, null, 2));
+
+  // just a check to make sure that all posts are included.
+  // Check those colected from the categories against those
+  // listed in `postFilenames` which has been populated
+  // from the posts folders.
+
+  Object.values(catHash).forEach((cat) => {
+    cat.posts.forEach((post) => {
+      const parts = post.url.split('/');
+      const [, , y, m, d, s] = parts;
+      const fn = `${y}-${m}-${d}-${s}.html`;
+      if (Array.isArray(postFilenames[fn])) postFilenames[fn].push(cat.id);
+      else console.error('no está en postfilenames', fn);
+    });
+  });
+  console.log('===== posts in categories =========');
+  const postsInGeneral = catHash.general.posts;
+  for (const post in postFilenames) {
+    const l = postFilenames[post].length;
+    if (l !== 1) {
+      console.log(post, l, postFilenames[post]);
+    }
+
+    // If a post was listed in a specific category but also in 'general'
+    // remove it from the ''general' (unclassified) category.
+    if (l > 1 && postFilenames[post].includes('general')) {
+      const date = post.substring(0, 10);
+      const slug = post.substring(11).replace('.html', '');
+      console.log(date, slug);
+      postsInGeneral.some((p, i) => {
+        if (p.date === date && p.slug === slug) {
+          console.log('remove entry', p, ' at  ', i);
+          postsInGeneral.splice(i, 1);
+          return true;
+        }
+      });
+    }
+  }
+  console.log(
+    '=======catHash 400 ==========',
+    JSON.stringify(catHash, null, 2)
+  );
 }
