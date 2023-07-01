4b90dca46de7f9fbb6ad328ee5968724772be2fd
Satyam <satyam@satyam.com.ar>
2023-07-01 17:58:45 +0200

    Fixed a bunch of src and dest file constants.

--- _scripts/constants.mjs
+++ _scripts/constants.mjs
@@ -6,22 +6,26 @@ export const __dirname = dirname(fileURLToPath(import.meta.url));
 // Important: the reason for the | key is because it gets sorted after all alpha characters
 export const NO_SUBCAT_KEY = '|';
 
+const ROOT = join(__dirname, '..');
+
 const ASSETS = 'assets';
 export const TEMPLATES = 'templates';
 export const STYLES = 'styles';
 
-const ROOT = join(__dirname, '..');
 const SCRIPT = __dirname;
+const BLOG_SRC = join(ROOT, 'blogsrc');
+
 export const SRC_DIRS = {
   templates: join(SCRIPT, TEMPLATES),
-  srcPosts: join(ROOT, 'blogsrc'),
+  srcPosts: BLOG_SRC,
   styles: join(SCRIPT, ASSETS, STYLES),
-  images: join(ROOT, ASSETS, 'img'),
+  images: join(BLOG_SRC, ASSETS, 'img'),
   js: join(SCRIPT, ASSETS, 'js'),
 };
 
-const SITE_DIR = join(__dirname, '../docs/blog');
+const SITE_DIR = join(ROOT, 'docs/blog');
 const ASSETS_DIR = join(SITE_DIR, ASSETS);
+
 export const DEST_DIRS = {
   posts: SITE_DIR,
   styles: join(ASSETS_DIR, 'css'),
