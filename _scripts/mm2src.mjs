8467f509a923d6da62bc7b5820330b7677bffa62
Satyam <satyam@satyam.com.ar>
2024-05-17 15:41:34 +0200

    In `mm2src` for `mitos` and `milagros`, - read all articles first to collect all info. -  create an index of all articles. - when processing the articles, if the filename is `index` add the index. - Index files get dated on Jan 2nd so they appear on top of the resto, that are date Jan 1st.

--- _scripts/mm2src.mjs
+++ _scripts/mm2src.mjs
@@ -17,42 +17,65 @@ const convert = async (name, category) => {
   });
   const outDir = join(SRC_DIRS.srcPosts, name);
   await ensureDir(outDir);
-  //   ---
-  // title: "El entierro de la sardina"
-  // date: "2003-03-05"
-  // categories: ["Viajes/Tres Cantos"]
-  // language: "es-ES"
-  // ---
-  console.log(name, category);
-  for (const srcFile of srcPostNames) {
+
+  const postsArray = srcPostNames.map((srcFile) => {
     const fMat = matter.read(join(srcFolder, srcFile));
     const title = fMat.data.title;
     const slug = slugify(title, { lower: true, strict: true });
-    const content = htmlParse(fMat.content);
-    const anchors = content.querySelectorAll('a');
+    return {
+      srcFile,
+      title,
+      slug,
+      content: fMat.content,
+    };
+  });
+
+  const index = () => {
+    if (postsArray.length) {
+      return d`
+        <ul>
+          ${postsArray
+            .map(
+              ({ slug, title }) => d`
+                <li><a href="./2000/01/01/${slug}.html">
+                  ${title}
+                </a></li>
+              `
+            )
+            .join('')}
+        </ul>
+      `;
+    }
+    return '';
+  };
+  for (const { srcFile, title, slug, content } of postsArray) {
+    const dom = htmlParse(content);
+    const anchors = dom.querySelectorAll('a');
     if (anchors.length) {
       console.log('a', name, srcFile);
       console.log(anchors.map((a) => a.getAttribute('href')));
     }
-    const breaks = content.querySelectorAll('br');
+    const breaks = dom.querySelectorAll('br');
     if (breaks.length) {
       console.log('br', name, srcFile, breaks.length);
     }
-    const emptyPs = content.querySelectorAll('p:empty');
+    const emptyPs = dom.querySelectorAll('p:empty');
     for (const p of emptyPs) {
       p.remove();
     }
 
+    const isIndex = srcFile.includes('index');
     await writeFile(
       `${join(outDir, slug)}.html`,
       d`
       ---
       title: "${title}"
-      date: "2000-01-01"
+      date: "${isIndex ? '2000-01-02' : '2000-01-01'}"
       categories: ["${category}"]
       language: "es-ES"
       ---
-      ${content.outerHTML}
+      ${dom.outerHTML}
+      ${isIndex ? index() : ''}
     `
     );
   }
